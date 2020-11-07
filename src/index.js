0813c52054eaebb5b282100912bedf86edb470c9
Allison Dietz <dietz.allison@outlook.com>
2020-11-06 23:27:33 -0700

    Begin building basic structure in HTML display initial inputs, validate values to begin fetching data

--- src/index.js
+++ src/index.js
@@ -8,3 +8,49 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+import User from './User'
+
+
+let usernameInput = document.querySelector('#username-input');
+let userPasswordInput = document.querySelector('#user-password-input');
+let loginSubmitButton = document.querySelector('.login-submit-button');
+
+let userData;
+
+loginSubmitButton.addEventListener('click', verifyLoginInputs);
+
+function verifyLoginInputs() {
+  if(!usernameInput.value) {
+    alert('Please input a valid username! (ex: customer[ user ID 1-50 ])')
+  } else if (userPasswordInput.value !== 'overlook2020') {
+    alert('Please input a valid passoword!')
+  } else if (usernameInput.value.length > 6 && userPasswordInput.value === 'overlook2020') {
+    submitFetch();
+  };
+};
+
+
+function submitFetch() {
+
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+    .then(response => response.json())
+    .then(data => makeUsers(data))
+    .then(users => console.log('users:', users))
+}
+
+function makeUsers(data) {
+
+  return data.users.map(userInfo => {
+    return new User(userInfo);
+  })
+
+}
+
+
+// SUBMIT FETCH REQUEST to get data from the API
+// Instatiate the correct type of USER,
+// DISPLAY the correct dashboard,
+//
+
+//
